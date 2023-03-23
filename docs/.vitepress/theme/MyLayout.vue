827464754ad74ffc4e107a3ed98f277ab0d7b15a
码路芽子 <mlyz.wdy@qq.com>
2023-03-23 14:43:52 +0800

    【fix】修改评论

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -2,7 +2,7 @@
 <template>
   <Layout>
     <template #doc-after>
-      <div class="comment-container"></div>
+      <div id="gitalk-container"></div> 
     </template>
   </Layout>
 </template>
@@ -27,16 +27,10 @@ const emptyNode = (node) => {
 
 const initGitalk = () => {
   if (typeof window !== undefined) {
-    const s_div = document.createElement('div'); // 创建节点
-    s_div.setAttribute('id', 'gitalk-page-container'); // 设置id
-    const container = document.querySelector('.comment-container'); // querySelector的节点可自己根据自己想加载的地方设置
-    if (container) {
-      emptyNode(container);
-      container.appendChild(s_div);
       const gitment = new Gitalk({
         id: { name: `${location.pathname.replace(/\W/g, '')}` }, // 可选。默认为 location.href
         owner: 'mlyz-wdy', // GitHub repository 所有者
-        repo: 'blog', // GitHub repository
+        repo: 'blog-comment', // GitHub repository
         clientID: '8d75bd3d346537698e1e', // 自己的clientID
         clientSecret: 'ab6651df300fb17af64bb5bb18fbef4baf6d1a31', // 自己的clientSecret
         admin: ['mlyz-wdy'], // GitHub repository 所有者
@@ -44,8 +38,7 @@ const initGitalk = () => {
         createIssueManually: true, //如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
         // proxy: "https://vercel.younglina.top/github_access_token",
       });
-      gitment.render('gitalk-page-container');
-    }
+      gitment.render('gitalk-container');
   }
 };
 onMounted(initGitalk);
